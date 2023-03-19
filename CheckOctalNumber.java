ed90375039a875ba0caeec615219223ef0d3c967
sathish <ssmanikandan150701@gmail.com>
2023-03-19 11:04:06 +0530

    java programes

---
+++ CheckOctalNumber.java
@@ -0,0 +1,12 @@
+package Payilagam;
+
+public class CheckOctalNumber {
+   public static void main(String[] args) {
+	int[] x={100,200,01111};
+	for (int i=0;i<x.length;i++){
+		System.out.println(x[i]+" ");
+	}
+	int var=077;
+	System.out.println(var);
+}
+}
